2f0424530e086ba07a8f0c99469a8ffe317c3504
anukuljoshi <anukul.joshi@geeksforgeeks.org>
2024-03-16 00:40:54 +0530

    added surround plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -133,5 +133,15 @@ return {
     lazy = true,
     config = true,
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  }
 }
 

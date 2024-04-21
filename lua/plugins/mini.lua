feef63dbe89c47658c40b0cfc8df549d181bc1e7
anukuljoshi <anukuljoshi1@gmail.com>
2024-04-21 12:15:54 +0530

    clean mini plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -6,17 +6,7 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     config = function()
-      -- Better Around/Inside textobjects
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [']quote
-      --  - ci'  - [C]hange [I]nside [']quote
-
-      require('mini.ai').setup { n_lines = 500 }
-
       -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
       local statusline = require('mini.statusline')
       statusline.setup()
 
@@ -27,42 +17,6 @@ return {
       statusline.section_location = function()
         return ''
       end
-
-      require("mini.files").setup({
-        -- Module mappings created only inside explorer.
-        -- Use `''` (empty string) to not create one.
-        mappings = {
-          close       = '<leader>e',
-          go_in       = 'l',
-          go_in_plus  = '<CR>',
-          go_out      = 'h',
-          go_out_plus = 'H',
-          reset       = '<BS>',
-          reveal_cwd  = '@',
-          show_help   = 'g?',
-          synchronize = '=',
-          trim_left   = '<',
-          trim_right  = '>',
-        },
-      })
-
-      local miniOpenCurrent = function()
-        local name = vim.api.nvim_buf_get_name(0)
-        MiniFiles.open(name)
-
-        if name ~= '' then
-          local depth = -2
-          local _, init = name:find(vim.pesc(vim.loop.cwd()))
-          while init do
-            depth = depth + 1
-            init = name:find('/', init + 1)
-          end
-
-          for _ = 1, depth do MiniFiles.go_out() end
-          for _ = 1, depth do MiniFiles.go_in() end
-        end
-      end
-      vim.keymap.set("n", "<leader>e", miniOpenCurrent, { desc = "open file explorer"})
     end,
   },
 
